72408e08bcfdbec4eb3c478995dfb18793a40ac5
hackjap <hackjap1015@gmail.com>
2021-07-11 10:17:09 +0900

    :sparkles:회원 레포지토리 개발

---
+++ book-shop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findALl() {
+        return em.createQuery("select  m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name",name) // ( db 컬럼과 바인딩되는 파라미터 명, 함수 파라미터 )
+                .getResultList();
+    }
+}
